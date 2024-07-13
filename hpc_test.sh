f421085e12895be02568639cf9f378d6361fd01c
Tanmay Pandit <t.m.pandit@student.tudelft.nl>
2024-07-13 09:36:17 +0200

    Added sbatch test script

---
+++ hpc_test.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+#SBATCH --job-name="test util_2.4"
+#SBATCH --partition=compute
+#SBATCH --time=00:30:00
+#SBATCH --ntasks=100   
+#SBATCH --account=education-eemcs-msc-es
+
+# Define the multiplied utilization values
+utils=(2.4)
+
+# Loop over each utilization value
+for util in "${utils[@]}"; do
+  # Convert the utilization value to the required format for the directory name
+  util_formatted=$(printf "%.2f-util" "$util")
+
+  # Loop over each jobset index from 0 to 9
+  for jobset_index in {0..99}; do
+    # Construct the jobset file name
+    jobset_file="100-jitter/${util_formatted}/jobsets/jobset-log-uniform-discrete_${jobset_index}.csv"
+
+    # Execute the command
+    srun build/nptest "$jobset_file" -m 4 -f "0.74,0.8,0.87,0.94,1.0" $
+  done
+done
+wait
