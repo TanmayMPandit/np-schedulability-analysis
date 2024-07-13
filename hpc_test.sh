58b79abfab6ec0728ffd32a6707dac9eac55f4e9
Tanmay Pandit <t.m.pandit@student.tudelft.nl>
2024-07-13 10:03:40 +0200

    updated hpc script

--- hpc_test.sh
+++ hpc_test.sh
@@ -3,7 +3,7 @@
 #SBATCH --job-name="test util_2.4"
 #SBATCH --partition=compute
 #SBATCH --time=00:30:00
-#SBATCH --ntasks=25
+#SBATCH --ntasks=1
 #SBATCH --cpus-per-task=1
 #SBATCH --mem-per-cpu=2G
 #SBATCH --account=education-eemcs-msc-es
