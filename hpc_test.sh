aeaaf3bd869b8e19a5c43ac437ec26a3c7989e0f
Tanmay Pandit <t.m.pandit@student.tudelft.nl>
2024-07-13 10:11:29 +0200

    Updated HPC submit shell

--- hpc_test.sh
+++ hpc_test.sh
@@ -8,21 +8,14 @@
 #SBATCH --mem-per-cpu=2G
 #SBATCH --account=education-eemcs-msc-es
 
-# Define the multiplied utilization values
-utils=(2.4)
 
-# Loop over each utilization value
-for util in "${utils[@]}"; do
-  # Convert the utilization value to the required format for the directory name
-  util_formatted=$(printf "%.2f-util" "$util")
+# Loop over each jobset index from 0 to 9
+for jobset_index in {0..99}; do
+  # Construct the jobset file name
+  jobset_file="100-jitter/2.40-util/jobsets/jobset-log-uniform-discrete_${jobset_index}.csv"
 
-  # Loop over each jobset index from 0 to 9
-  for jobset_index in {0..99}; do
-    # Construct the jobset file name
-    jobset_file="100-jitter/${util_formatted}/jobsets/jobset-log-uniform-discrete_${jobset_index}.csv"
-
-    # Execute the command
-    srun build/nptest "$jobset_file" -m 4 -f "0.74,0.8,0.87,0.94,1.0" &
-  done
+  # Execute the command
+  srun build/nptest "$jobset_file" -m 4 -f "0.74,0.8,0.87,0.94,1.0" &
 done
+
 wait
