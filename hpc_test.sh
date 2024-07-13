edd4bff774f1c1aa6ac3fb0ef42eee1a9e849b9c
Tanmay Pandit <t.m.pandit@student.tudelft.nl>
2024-07-13 12:45:22 +0200

    trying to run shell with sbatch

--- hpc_test.sh
+++ hpc_test.sh
@@ -3,19 +3,10 @@
 #SBATCH --job-name="test util_2.4"
 #SBATCH --partition=compute
 #SBATCH --time=00:30:00
-#SBATCH --ntasks=25
-#SBATCH --cpus-per-task=1
-#SBATCH --mem-per-cpu=2G
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=25
+#SBATCH --mem-per-cpu=1G
 #SBATCH --account=education-eemcs-msc-es
 
 
-# Loop over each jobset index from 0 to 9
-for jobset_index in {0..99}; do
-  # Construct the jobset file name
-  jobset_file="100-jitter/2.40-util/jobsets/jobset-log-uniform-discrete_${jobset_index}.csv"
-
-  # Execute the command
-  srun build/nptest "$jobset_file" -m 4 -f "0.74,0.8,0.87,0.94,1.0" &
-done
-
-wait
+srun test.sh
