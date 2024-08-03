856d1681b9bf1446ca8c78813ef29b615e820515
Tanmay Pandit <t.m.pandit@student.tudelft.nl>
2024-08-03 15:13:05 +0200

    Added dedicated hpc script

---
+++ experiment scripts/exp_1/hpc_distribution.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#SBATCH --job-name="Exp_1:distribution"
+#SBATCH --partition=compute
+#SBATCH --time=01:30:00
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=40
+#SBATCH --mem-per-cpu=1G
+#SBATCH --account=education-eemcs-msc-es
+
+
+srun distribution_based.sh
--- experiment scripts/exp_1/hpc_single.sh
+++ experiment scripts/exp_1/hpc_single.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-#SBATCH --job-name="test util_2.4"
+#SBATCH --job-name="Exp_1:single"
 #SBATCH --partition=compute
-#SBATCH --time=00:30:00
+#SBATCH --time=01:30:00
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=25
+#SBATCH --cpus-per-task=40
 #SBATCH --mem-per-cpu=1G
 #SBATCH --account=education-eemcs-msc-es
 
 
-srun test.sh
+srun single_speed.sh
