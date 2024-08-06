ca1ab8ac9ba5f8befcc3662c1a8b8448cafed13b
Tanmay Pandit <t.m.pandit@student.tudelft.nl>
2024-08-06 17:43:56 +0200

    exp_2 threadpool scripts

---
+++ threadpool_scripts/exp_2/exp_2.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+sbatch exp_2_hpc_dis.sh
+sbatch exp_2_hpc_search.sh
---
+++ threadpool_scripts/exp_2/exp_2_hpc_search.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#SBATCH --job-name="Exp_2:Search"
+#SBATCH --partition=compute
+#SBATCH --time=02:30:00
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=40
+#SBATCH --mem-per-cpu=2G
+#SBATCH --account=education-eemcs-msc-es
+
+module load 2022r2
+module load python/3.8.12
+
+srun  python exp_2_search.py
