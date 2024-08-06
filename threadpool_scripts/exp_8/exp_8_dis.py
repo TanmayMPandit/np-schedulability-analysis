bc97f99ad7bccebeab975b8b5e6829155acc705e
Tanmay Pandit <t.m.pandit@student.tudelft.nl>
2024-08-06 21:16:53 +0200

    exp_8 threadpool script

---
+++ threadpool_scripts/exp_8/exp_8_dis.py
@@ -0,0 +1,54 @@
+import os
+from pathlib import Path
+import subprocess
+import multiprocessing
+
+# Define the array of execution ratios
+execution_ratios = [0.4, 0.5, 0.7, 0.8, 0.9, 1.0]
+
+# Number of cores to use
+num_cores = 40
+
+# Function to run the command
+def run_command(jobset_file, ratio):
+    command = [
+        "build/nptest",
+        jobset_file,
+        "-m", "4",
+        "-f", "0.74,0.80,0.87,0.94,1.00",
+        "--energy-timeout", "3600",
+        "-o", "results/exp_8",
+        "-u", str(ratio)
+    ]
+    try:
+        subprocess.run(command, check=True)
+    except subprocess.CalledProcessError as e:
+        print(f"Command {command} failed with error: {e}")
+
+# List to hold all job sets
+job_sets = []
+
+# Loop through each execution ratio
+for ratio in execution_ratios:
+    jobset_dir = f"exp_8/{ratio}/rand-fixed-sum-utilDist/log-uniform-discrete-perDist/4-core/6-task/100-jitter/1.60-util/jobsets"
+    
+    # Check if the directory exists
+    jobset_path = Path(jobset_dir)
+    if jobset_path.exists() and jobset_path.is_dir():
+        for jobset_file in jobset_path.iterdir():
+            if jobset_file.is_file():
+                job_sets.append((str(jobset_file), ratio))
+            else:
+                print(f"{jobset_file} is not a file.")
+    else:
+        print(f"Directory {jobset_dir} does not exist.")
+
+# Function to process jobs using multiprocessing
+def process_jobs():
+    with multiprocessing.Pool(processes=num_cores) as pool:
+        pool.starmap(run_command, job_sets)
+
+if __name__ == "__main__":
+    # print(f"Current working directory: {os.getcwd()}")
+    # print(f"Using {num_cores} cores for multiprocessing")
+    process_jobs()
---
+++ threadpool_scripts/exp_8/exp_8_hpc_dis.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#SBATCH --job-name="Exp_8:Distribution"
+#SBATCH --partition=compute
+#SBATCH --time=03:00:00
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=40
+#SBATCH --mem-per-cpu=1G
+#SBATCH --account=education-eemcs-msc-es
+
+module load 2022r2
+module load python/3.8.12
+
+srun  python exp_8_dis.py
