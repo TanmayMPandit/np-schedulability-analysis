69a93c435d6889275b7e9036e8cbb4ec7ee50bd0
Tanmay Pandit <t.m.pandit@student.tudelft.nl>
2024-08-16 09:11:43 +0200

    added dedicated scripts for 10 core

---
+++ threadpool_scripts/exp_7/10_core/exp_7_hpc_search_1.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#SBATCH --job-name="Exp_7:Search_1"
+#SBATCH --partition=compute
+#SBATCH --time=24:00:00
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=20
+#SBATCH --mem=180G
+#SBATCH --account=education-eemcs-msc-es
+
+module load 2022r2
+module load python/3.8.12
+
+srun  python exp_7_search_1.py
---
+++ threadpool_scripts/exp_7/10_core/exp_7_hpc_single_2_2.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#SBATCH --job-name="Exp_7:Single_2"
+#SBATCH --partition=compute
+#SBATCH --time=24:00:00
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=20
+#SBATCH --mem=180G
+#SBATCH --account=education-eemcs-msc-es
+
+module load 2022r2
+module load python/3.8.12
+
+srun  python exp_7_single_2_2.py
